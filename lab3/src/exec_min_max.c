5ebad5f6dac0db8bf1fd7458b7e62f4d4fa1561f
glebbez <glebbez@mail.ru>
2020-05-01 17:12:59 +0000

    5 task

--- lab3/src/exec_min_max.c
+++ lab3/src/exec_min_max.c
@@ -1,42 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-int main(int argc, char **argv)
+int main(void)
 {
-    printf("%s\n",argv[0] );
-    printf("%s\n",argv[1] );
-    printf("%s\n",argv[2] );
-    printf("\n" );
-	pid_t pid = fork();
-
-    
-	if ( pid >= 0 ) 
-    {
-	    if(pid==0)
-        {
-    		int c = execv( "parallel_min_max", argv);
-    		if(c == -1)
-    		{
-    		    printf("Exec error\n");
-    		    return 1;
-    		}
-	   }
-	}
-	else 
-    {
-	    printf("Fork fault\n");
-	    return 1;
-	}
-	
-    int status;
-	wait(&status);
-    if(!WIFEXITED(status))
-    {
-        printf("Error exiting child process\n");
-        return 1;
-    }
+   execl("sequential_min_max"," ","100","10",NULL);
 	return 0;
 }
 
