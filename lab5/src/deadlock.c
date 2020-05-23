24a2db0e3b81a2daab5e22776715bf414962cd8b
glebbez <glebbez@mail.ru>
2020-05-23 12:54:03 +0000

    all is done

--- lab5/src/deadlock.c
+++ lab5/src/deadlock.c
@@ -3,25 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define ERROR_CREATE_THREAD -11
-#define ERROR_JOIN_THREAD   -12
-#define SUCCESS             0
-
 pthread_mutex_t mut_1, mut_2;
 
-int test = 0;
-
-void *proc_0()
+void *process1()
 {
-    printf("\nThis is proc_0 using rs1");
+    printf("\nProcess A is using x");
     pthread_mutex_lock(&mut_1);
-    
         sleep(1);
-        
-        printf("\np1 trying to get rs2..."); 
+        printf("\nProcess A wants to use y"); 
         pthread_mutex_lock(&mut_2);
-            test++;
-        printf("\nproc1 got rs2!!");    
+
+        printf("\nProcess A can use y now");    
         pthread_mutex_unlock(&mut_2);   
 		
     pthread_mutex_unlock(&mut_1);  
@@ -29,17 +21,17 @@ void *proc_0()
 return 0;
 }
 
-void *proc_1()
+void *process2()
 {
-    printf("\nThis is proc_1 using rs2");
+    printf("\nProcess B is using y");
     pthread_mutex_lock(&mut_2);
     
         sleep(1);
         
-        printf("\np2 trying to get rs1..."); 
+        printf("\nProcess B wants to use x"); 
         pthread_mutex_lock(&mut_1);
-            test--;
-        printf("\nproc2 got rs1!!");    
+
+        printf("\nProcess B can use x now");    
         pthread_mutex_unlock(&mut_1);  
 		
     pthread_mutex_unlock(&mut_2);  
@@ -51,30 +43,12 @@ return 0;
 int main(){
     
     pthread_t threads[2];
-    int status;
+
+    pthread_create(&threads[0], NULL, process1, NULL);
+    pthread_create(&threads[1], NULL, process2, NULL);
     
-    status = pthread_create(&threads[0], NULL, proc_0, NULL);
-    
-    if (status != SUCCESS) {
-        printf("Main error: can't create threads[0], status = %d\n", status);
-        return ERROR_CREATE_THREAD;
-    }
-    
-    status = pthread_create(&threads[1], NULL, proc_1, NULL);
-    if (status !=SUCCESS) {
-        printf("Main error: can't create threads[1], status = %d\n", status);
-        return ERROR_CREATE_THREAD;
-    }
-    status = pthread_join(threads[0], NULL);
-    if (status != SUCCESS) {
-        printf("Main error: can't join threads[0], status = %d\n", status);
-        return ERROR_JOIN_THREAD;
-    }
-    status = pthread_join(threads[1], NULL);
-    if ( status != SUCCESS) {
-        printf("Main error: can't join threads[1], status = %d\n", status);
-        return ERROR_JOIN_THREAD;
-    }
-    
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+
     return 0;
 }
